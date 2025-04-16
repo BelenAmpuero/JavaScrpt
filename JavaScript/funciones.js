d9bc727357aa9ab8390a4d649fd9b8757ea451ff
BelenAmpuero <bnampuero@gmail.com>
2025-04-16 18:25:38 -0300

    variables

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -3,3 +3,21 @@ function sumaTres(x) {
 }
 
 sumaTres(5)
+
+// FORMAS DE DECLARAR UNA FUNCION
+// PRIMERO
+
+function sumaTres(x) {
+    return x + 3;
+}
+
+// SEGUNDO
+
+var sumaTres = function(x) {
+    return x + 3;
+};
+
+// TERCERO
+var sumaTres = (x) => {
+    return x + 3;
+};

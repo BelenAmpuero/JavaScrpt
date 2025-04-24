8a82d327de09bdad560b0909f150d8ca700a7324
BelenAmpuero <bnampuero@gmail.com>
2025-04-24 18:43:45 -0300

    Agg Bucles y operadores

---
+++ JavaScript/operadoresdeincremento.js
@@ -0,0 +1,13 @@
+let i = 5;
+let a = i++;
+
+console.log("a =", a);
+console.log("i =", i); //Primero devuelve el valor original. Luego suma 1
+
+let j = 5;
+let b = ++j; 
+
+console.log("b =", b);
+console.log("j=", j); // Primero suma 1, luego devuelve el valor
+
+
--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -12,7 +12,6 @@ var numeroDecimal = 11.75;
 // Booleanos
 var estoyEntendiendo = true;
 var estoyAburrido = false;
-
 // Undefined
 var cajon1;
 console.log(cajon1);
